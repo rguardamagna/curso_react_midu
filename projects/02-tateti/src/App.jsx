c593b6e648bd6bc76377c6f9e1ce286deb1e0ddc
rguardamagna <r_guardamagna@hotmail.com>
2023-09-17 19:31:04 +0200

    firtCommit

---
+++ projects/02-tateti/src/App.jsx
@@ -0,0 +1,52 @@
+import {useState} from 'react'
+
+const TURNS = {
+  X:'x',
+  O:'o',
+}
+
+
+const Square = ({children, isSelected, index, updateBoard}) => {
+
+  const className =`square ${isSelected ? 'is-selected': ''}`
+  
+  return (
+    <div className={className}>
+      {children}
+    </div>
+  )
+}
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState[TURNS.X]
+
+  return (
+    <main className='board'>
+      <h1>Ta Te Ti</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square key={index} index={index}>
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn===TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn===TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+    </main>
+  )
+}
+
+export default App
