34ca9e267080cedf2f8008490e443add7c7d4996
rguardamagna <r_guardamagna@hotmail.com>
2023-09-17 17:20:06 +0200

    fifthCommit

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,30 +1,59 @@
-import {useState} from 'react'
+//import {useState} from 'react'
 import { TwitterFollowCard } from './twitterFollowCard'
 export function App() {
     
     //const formattedUserName = (<span>@Rodrigo</span>) // Esto es un elemento y puede pasarse directamente a la tarjeta
     //const formatUserName = (userName) => `@${userName}`
     // userName = `@${userName} // Modificar la prop es una mala praxis, si es necesario, crear una cost diferente
-    
-    const [name, setName] = useState('Rodrigo')
+    // const [name, setName] = useState('Rodrigo')
+
+    const users = [
+        {
+            userName: 'midudev',
+            name: 'Migue Angel Durán',
+            isFollowing: true,
+        },
+        {
+            userName: 'pheralb' ,
+            name: 'Pablo H.',
+            isFollowing: false,
+        },
+        {
+            userName: 'Pablo Hdezs',
+            name: 'Paco Hdez',
+            isFollowing: true,
+        },
+        {
+            userName: 'TMChein',
+            name: 'Tomas',
+            isFollowing: false,
+        },
+    ]
 
     return (
         <section className='App'>
             {/* <TwitterFollowCard formatUserName = {formatUserName} isFollowing={true} userName="kikobeats" name='Rodrigo Guardamagna'/> */}
             {/* <TwitterFollowCard isFollowing={true} userName="midudev" name='Miguel Angel Duran'/>
             <TwitterFollowCard isFollowing={false} userName="elonmusk" name='Elon Musk'/>
-            <TwitterFollowCard isFollowing userName="vxnder" name='VanderHart'/> */}
-            
-            <TwitterFollowCard isFollowing userName="midudev">
+            <TwitterFollowCard isFollowing userName="vxnder" name='VanderHart'/>
+            <TwitterFollowCard isFollowing userName={name}>
                 <strong>Miguel Angel Duran</strong>
-            </TwitterFollowCard>
-            <TwitterFollowCard isFollowing={false} userName="vxnder">
-                <strong>{name}</strong>
-            </TwitterFollowCard>
+            </TwitterFollowCard> */}
             
-            <button onClick={() => setName('Guardamagna')}>
+            {
+                users.map (({userName, name, isFollowing}) => {
+                    return (
+                    <TwitterFollowCard key={userName} userName={userName} initialIsFollowing={isFollowing}>
+                        <strong>{name}</strong>
+                    </TwitterFollowCard>
+
+                    )
+                })
+            }
+
+            {/* <button onClick={() => setName('Guardamagna')}>
                 Cambio Nombre
-            </button>
+            </button> */}
 
         </section>
     )
--- projects/00-hola-mundo/src/twitterFollowCard.jsx
+++ projects/00-hola-mundo/src/twitterFollowCard.jsx
@@ -2,13 +2,13 @@ import {useState} from 'react'
 
 import './App.css'
 //export function TwitterFollowCard ({formatUserName, userName, name, isFollowing}) 
-export function TwitterFollowCard ({children, formatUserName, userName = 'unknown', name})
+export function TwitterFollowCard ({children, userName = 'unknown', initialIsFollowing})
 {
     // const state = useState(false)
     // const isFollowing = state[0]
     // const setIsFollowing = state[1]
     // lo de arriba es igual a:
-    const [isFollowing, setIsFollowing] = useState(false)
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
 
     //const addAt = (userName) => `@${userName}`
     const text = isFollowing ? 'Siguiendo' : 'Seguir'
@@ -37,7 +37,8 @@ export function TwitterFollowCard ({children, formatUserName, userName = 'unknow
     
                 <aside>
                     <button className={buttonClassName} onClick={handleClick}>
-                        {text}
+                        <span className='tw-follow-card-text'>{text}</span>
+                        <span className='tw-follow-card-stopFollow'>Dejar de seguir</span>
                     </button>
                 </aside>
             </article>
