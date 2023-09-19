2978869fd81f6bce710c4e999dc899589e820e85
rguardamagna <r_guardamagna@hotmail.com>
2023-09-19 17:26:00 +0200

    fourthCommit

--- projects/02-tateti/src/App.jsx
+++ projects/02-tateti/src/App.jsx
@@ -10,17 +10,74 @@ const Square = ({children, isSelected, index, updateBoard}) => {
 
   const className =`square ${isSelected ? 'is-selected': ''}`
   
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
   return (
-    <div className={className}>
+    <div onClick={handleClick}className={className}>
       {children}
     </div>
   )
 }
 
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,6],
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
 
-  const [turn, setTurn] = useState[TURNS.X]
+  const [turn, setTurn] = useState(TURNS.X)
+
+  // null es que no hay ganador, false es que hay un empate
+  const [winner, setWinner] = useState(null) 
+
+  const checkWinner = (boardToCheck) => {
+    
+    // revisamos todas las combinaciones ganadoras, para ver si X o O ganaron
+    for (const combo of WINNER_COMBOS) {
+      const [a,b,c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+  //si no hay ganador
+  return null
+  }
+  
+  const updateBoard = (index) => {
+    
+    // no actualizar el cuadro si ya tiene un valor dentro
+    if (board[index] || winner) return
+
+    // actualizar el tablero
+    const newBoard = [... board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    // cambiar el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  
+    // revisar si hay un ganador
+    const newWinner = checkWinner(newBoard) 
+    if (newWinner) {
+      setWinner(newWinner)
+    }
+  
+  }
 
   return (
     <main className='board'>
@@ -29,7 +86,7 @@ function App() {
         {
           board.map((_, index) => {
             return (
-              <Square key={index} index={index}>
+              <Square key={index} index={index} updateBoard={updateBoard}>
                 {board[index]}
               </Square>
             )
