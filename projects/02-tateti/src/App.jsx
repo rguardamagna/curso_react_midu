62653339533fe098a9537cb56e65dab4c559d401
rguardamagna <r_guardamagna@hotmail.com>
2023-09-20 16:51:14 +0200

    sixthCommit

--- projects/02-tateti/src/App.jsx
+++ projects/02-tateti/src/App.jsx
@@ -1,62 +1,34 @@
 import {useState} from 'react'
+import confetti from 'canvas-confetti'
 
-const TURNS = {
-  X:'x',
-  O:'o',
-}
-
-
-const Square = ({children, isSelected, index, updateBoard}) => {
-
-  const className =`square ${isSelected ? 'is-selected': ''}`
-  
-  const handleClick = () => {
-    updateBoard(index)
-  }
-
-  return (
-    <div onClick={handleClick}className={className}>
-      {children}
-    </div>
-  )
-}
-
-const WINNER_COMBOS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,4,8],
-  [2,4,6],
-  [0,3,6],
-  [1,4,7],
-  [2,5,6],
-]
+import { Square } from "./components/Square"
+import {TURNS} from "./constasts"
+import {checkWinnerFrom, checkEndGame} from "./logic/board"
+import {WinnerModal} from "./components/WinnerModal"
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
 
-  const [turn, setTurn] = useState(TURNS.X)
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
 
   // null es que no hay ganador, false es que hay un empate
   const [winner, setWinner] = useState(null) 
 
-  const checkWinner = (boardToCheck) => {
-    
-    // revisamos todas las combinaciones ganadoras, para ver si X o O ganaron
-    for (const combo of WINNER_COMBOS) {
-      const [a,b,c] = combo
-      if (
-        boardToCheck[a] &&
-        boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck[c]
-      ) {
-        return boardToCheck[a]
-      }
-    }
-  //si no hay ganador
-  return null
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
-  
+
   const updateBoard = (index) => {
     
     // no actualizar el cuadro si ya tiene un valor dentro
@@ -70,11 +42,17 @@ function App() {
     // cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // Guardar la partida
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', JSON.stringify(turn))
   
     // revisar si hay un ganador
-    const newWinner = checkWinner(newBoard) 
+    const newWinner = checkWinnerFrom(newBoard) 
     if (newWinner) {
+      confetti()
       setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
     }
   
   }
@@ -82,12 +60,15 @@ function App() {
   return (
     <main className='board'>
       <h1>Ta Te Ti</h1>
+      <button onClick={resetGame}>
+        Resetear juego
+      </button>
       <section className="game">
         {
-          board.map((_, index) => {
+          board.map((square, index) => {
             return (
               <Square key={index} index={index} updateBoard={updateBoard}>
-                {board[index]}
+                {square}
               </Square>
             )
           })
@@ -102,6 +83,7 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
+      <WinnerModal resetGame={resetGame} winner={winner}></WinnerModal>
     </main>
   )
 }
---
+++ projects/02-tateti/src/components/WinnerModal.jsx
@@ -0,0 +1,29 @@
+import { Square } from "./Square"
+
+export function WinnerModal ({winner, resetGame}) {
+    if (winner !== null)
+
+    return (
+        <section className='winner'>
+            <div className='text'>
+                <h2>
+                    {
+                    winner === false
+                    ? 'Empate'
+                    : 'Ganó: '
+                    }
+                </h2>
+                <header className='win'>
+                    {winner && <Square>{winner}</Square>}
+                </header>
+
+                <footer>
+                    <button onClick={resetGame}>
+                        Empezar de nuevo
+                    </button>
+                </footer>
+            </div>
+        </section>
+        )
+    }
+    
