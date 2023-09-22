f7e3c712fa306af17f6814c314fa67ab9ed638a5
rguardamagna <r_guardamagna@hotmail.com>
2023-09-22 12:41:39 +0200

    secondCommitProj03

--- projects/03-mouse-follow/src/App.jsx
+++ projects/03-mouse-follow/src/App.jsx
@@ -1,15 +1,30 @@
 import { useState, useEffect } from "react"
 
-function App() {
+const FollowMouse = () => {
   const [enabled,setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x:0 , y:0 })
 
   useEffect(() => {
-  console.log('efecto ', {enabled})
-}, [enabled])
+    
+    const handleMove = (event) => {
+      const {clientX, clientY} = event  
+      setPosition({x:clientX, y:clientY})
+    }
+    
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // función que permite limpiar las suscripciones a eventos (de lo contrario addEvent no se cierra)
+    // Se ejecuta cuando el componente se desmonta y cuando cambian las dependencias
+    return () => { // limpieza de suscripción
+      window.removeEventListener('pointermove', handleMove)
+      }
+  }, [enabled])
   return (
-    <main>
+    <>
       <div style = {{
-        postion:'absolute',
+        position:'absolute',
         backgroundColor:'#09f',
         borderRadius:'50%',
         opacity:0.8,
@@ -18,13 +33,23 @@ function App() {
         top: -20,
         width: 40,
         height: 40,
-        transform: 'translate(0px, 0px)'
-      }}>
-
-      </div>
-      <button
-        onClick={() => setEnabled(!enabled)}>
+        transform: `translate(${position.x}px, ${position.y}px)`,
+      }}/>
+      <button onClick={() => setEnabled(!enabled)}>
           {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </>
+  )
+}
+
+function App() {
+const [mounted, setMounted] = useState(true)
+
+  return (
+    <main>
+      {mounted && <FollowMouse/>}
+      <button onClick={() => setMounted(!mounted)}>
+        Toggle mounted FollowMouse
       </button>
     </main>
   )
